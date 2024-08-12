0222422be1abd979bbb4f566713150a921deaab7
b1ack-panther <saurabh8604vns@gmail.com>
2024-08-13 03:24:44 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,26 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	// res.setHeader("Access-Control-Allow-Origin", "*");
+	// another common pattern
+	res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET,OPTIONS,PATCH,DELETE,POST,PUT"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+	);
+	if (req.method === "OPTIONS") {
+		res.status(200).end();
+		return;
+	}
+	next();
+});
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
