a0e4b18a5658b503e7df6d4ef2906230eefe567e
b1ack-panther <saurabh8604vns@gmail.com>
2024-08-14 11:30:01 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -9,34 +9,27 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3333;
 
-// app.use((req, res, next) => {
-// 	res.setHeader("Access-Control-Allow-Credentials", true);
-// 	// res.setHeader("Access-Control-Allow-Origin", "*");
-// 	// another common pattern
-// 	res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-// 	res.setHeader(
-// 		"Access-Control-Allow-Methods",
-// 		"GET,OPTIONS,PATCH,DELETE,POST,PUT"
-// 	);
-// 	res.setHeader(
-// 		"Access-Control-Allow-Headers",
-// 		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-// 	);
-// 	if (req.method === "OPTIONS") {
-// 		res.status(200).end();
-// 		return;
-// 	}
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.setHeader(
+		"Content-Security-Policy",
+		"default-src 'none'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;"
+	);
+	next();
+});
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 dbConnection();
 
-app.get("^/$|/index(.html)?", (req, res) => {
-	res.sendFile(path.join(__dirname, "views", "index.html"));
-});
+// app.get("^/$|/index(.html)?", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "views", "index.html"));
+// });
+
+
+app.get("/", (req, res) => {
+	res.send("server is running.")
+})
 
 app.use("/user", require("./routes/userRoutes.js"));
 app.use("/project", require("./routes/projectRoutes.js"));
